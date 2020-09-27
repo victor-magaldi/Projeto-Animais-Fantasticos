a12fa224341f19335638c2c9d54820bdc1db3546
Victor de Souza Magaldi <victmagaldi@hotmail.com>
2020-09-27 20:45:48 -0300

    scroll suave javascript

--- script.js
+++ script.js
@@ -37,3 +37,29 @@ function initAccordion(){
 }
 initAccordion();
 
+
+function initScrollSuave(){
+    const linksInternos = document.querySelectorAll('.js-menu a[href^="#"]');
+
+    function scrollToSection(event){
+        event.preventDefault();
+        const href= event.currentTarget.getAttribute('href');
+        const section = document.querySelector(href);
+        const topo = section.offsetTop;
+
+        section.scrollIntoView({
+            behavior:'smooth',
+            block: 'start'
+        });
+        //forma alternativa de colcoar um scroll suave
+        // window.scrollTo({
+        //     top:topo,
+        //     behavior: "smooth"
+        // })
+    }
+
+    linksInternos.forEach((link)=>{
+        link.addEventListener('click', scrollToSection)
+    })
+}
+initScrollSuave();

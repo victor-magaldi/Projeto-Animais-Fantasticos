728657a966394d1cb57377593ae551ca98bc2279
Victor de Souza Magaldi <victmagaldi@hotmail.com>
2020-09-27 21:12:50 -0300

    anima o scroll

--- script.js
+++ script.js
@@ -63,3 +63,24 @@ function initScrollSuave(){
     })
 }
 initScrollSuave();
+
+
+function initAnimationScroll(){
+    const sections = document.querySelectorAll(".js-scroll");
+// animação acontecerá a partir de 65 % da tela
+    if(sections.length){
+        const windowmetade = window.innerHeight * 0.4;
+        function animaScroll(){
+            sections.forEach((section)=>{
+                const sectiontop = section.getBoundingClientRect().top - windowmetade;
+                const isSectionVisible = (sectiontop - windowmetade) < 0;
+                if(isSectionVisible){
+                    section.classList.add('ativo')
+                }
+            })
+        }
+    }
+    animaScroll();
+    window.addEventListener('scroll',animaScroll);
+}
+initAnimationScroll();

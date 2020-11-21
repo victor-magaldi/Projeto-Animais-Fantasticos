a64cea8b1089b441a0d1c4cfceb0b85ed8f9745a
Victor de Souza Magaldi <victmagaldi@hotmail.com>
2020-11-21 20:56:03 -0300

    inicia modal login

---
+++ js/modules/modal-login.js
@@ -0,0 +1,14 @@
+export default function initmodal(){
+
+}
+
+const btnAbrir = document.querySelector("[data-modal=abrir");
+const btnFechar = document.querySelector("[data-modal=fechar");
+const containerModal = document.querySelector("[data-modal=container");
+
+function abrirModal(){
+
+}
+function fecharModal(){
+    
+}

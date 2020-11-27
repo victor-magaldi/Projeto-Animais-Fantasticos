03c6161bc2997d1499dd48faafc9212c983c976c
Victor de Souza Magaldi <victmagaldi@hotmail.com>
2020-11-27 20:02:07 -0300

    conclui Modal

--- js/index.js
+++ js/index.js
@@ -2,8 +2,11 @@ import initScrollSuave from './modules/scroll-suave.js';
 import initAnimationScroll from './modules/anima-scroll.js';
 import initTabNav from './modules/nav-tabs.js';
 import initAccordion from './modules/nav-accordion.js';
+import initmodal from './modules/modal-login.js';
+
 
 initScrollSuave();
 initAnimationScroll();
 initAccordion();
 initTabNav();
+initmodal();
--- js/modules/modal-login.js
+++ js/modules/modal-login.js
@@ -1,14 +1,26 @@
 export default function initmodal(){
+    const btnAbrir = document.querySelector("[data-modal=abrir");
+    const btnFechar = document.querySelector("[data-modal=fechar");
+    const containerModal = document.querySelector("[data-modal=container");
+
+    if(btnAbrir && btnFechar &&containerModal){
+        function toggleModal(event){
+            event.preventDefault()
+            containerModal.classList.toggle("ativo")
+        
+        }
+        
+        function foraDoModal(event){
+            if(event.target === this){
+                containerModal.classList.remove("ativo")
+            }
+        }
+        btnAbrir.addEventListener("click",toggleModal)
+        btnFechar.addEventListener("click",toggleModal)
+        containerModal.addEventListener("click",foraDoModal)
+    }
+
 
 }
 
-const btnAbrir = document.querySelector("[data-modal=abrir");
-const btnFechar = document.querySelector("[data-modal=fechar");
-const containerModal = document.querySelector("[data-modal=container");
 
-function abrirModal(){
-
-}
-function fecharModal(){
-    
-}

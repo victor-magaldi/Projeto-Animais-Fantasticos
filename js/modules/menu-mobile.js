ef6a07fa35026225299fdd94af2894a355538785
Victor de Souza Magaldi <victmagaldi@hotmail.com>
2021-01-07 19:48:25 -0300

    finaliza menu mobile

--- js/modules/menu-mobile.js
+++ js/modules/menu-mobile.js
@@ -1,1 +1,21 @@
-export default function initMenuMobile() {}
+import outsideClick from "./outsideclick.js";
+
+export default function initMenuMobile() {
+   const menuButton = document.querySelector("[data-menu=button]");
+   const menuList = document.querySelector("[data-menu=list]");
+   const eventos = ["click", "touchstart"];
+
+   if (menuButton) {
+      function openMenu(event) {
+         menuList.classList.toggle("active");
+         menuButton.classList.toggle("active");
+         outsideClick(menuList, eventos, () => {
+            menuList.classList.toggle("active");
+            menuButton.classList.toggle("active");
+         });
+      }
+      eventos.forEach((evento) => {
+         menuButton.addEventListener(evento, openMenu);
+      });
+   }
+}

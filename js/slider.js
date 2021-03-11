128ca53995cb5db75e3378d9b61c292922c0ccbe
Victor de Souza Magaldi <victmagaldi@hotmail.com>
2021-03-11 20:53:39 -0300

    configuracoes slide

--- js/slider.js
+++ js/slider.js
@@ -10,11 +10,46 @@ class Slide {
       };
 
       this.init();
+      this.changeSlide();
+   }
+
+   // slides configs
+   slidePosition(slide) {
+      const margin = (this.wrapper.offsetWidth - slide.offsetWidth) / 2;
+      return -(slide.offsetLeft - margin);
+   }
+
+   slideConfig() {
+      this.slideArray = [...this.slide.children].map((element) => {
+         const position = this.slidePosition(element);
+         return {
+            position,
+            element,
+         };
+      });
+   }
+
+   slideIndexNav(index) {
+      const last = this.slideArray.length - 1;
+
+      this.index = {
+         prev: index !== 0 ? undefined : index - 1,
+         active: index,
+         next: index === last ? undefined : index + 1,
+      };
+   }
+
+   changeSlide(index) {
+      const activeSlide = this.slideArray[index];
+      this.moveSlide(activeSlide.position);
+      this.slideIndexNav(index);
+      this.dist.finalPosition = activeSlide.position;
    }
 
    init() {
       this.bindEvents();
       this.addSlideEvents();
+      this.slideConfig();
       return this;
    }
 

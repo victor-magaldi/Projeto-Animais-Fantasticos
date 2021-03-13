3ab99b85b26bb1f4e9c40589a2ba5f561fc1bf12
Victor de Souza Magaldi <victmagaldi@hotmail.com>
2021-03-13 17:31:05 -0300

    resolve bugs nav Next e Prev

--- js/slider.js
+++ js/slider.js
@@ -1,33 +1,89 @@
-class Slide {
+export default class Slide {
    constructor(slide, wrapper) {
       this.slide = document.querySelector(slide);
       this.wrapper = document.querySelector(wrapper);
-
-      this.dist = {
-         finalPosition: 0,
-         startX: 0,
-         movement: 0,
-      };
-
-      this.init();
-      this.changeSlide();
-      this.activeNextSlide();
+      this.dist = { finalPosition: 0, startX: 0, movement: 0 };
    }
 
-   // slides configs
+   transition(active) {
+      this.slide.style.transition = active ? "transform .3s" : "";
+   }
+
+   moveSlide(distX) {
+      this.dist.movePosition = distX;
+      this.slide.style.transform = `translate3d(${distX}px, 0, 0)`;
+   }
+
+   updatePosition(clientX) {
+      this.dist.movement = (this.dist.startX - clientX) * 1.6;
+      return this.dist.finalPosition - this.dist.movement;
+   }
+
+   onStart(event) {
+      let movetype;
+      if (event.type === "mousedown") {
+         event.preventDefault();
+         this.dist.startX = event.clientX;
+         movetype = "mousemove";
+      } else {
+         this.dist.startX = event.changedTouches[0].clientX;
+         movetype = "touchmove";
+      }
+      this.wrapper.addEventListener(movetype, this.onMove);
+      this.transition(false);
+   }
+
+   onMove(event) {
+      const pointerPosition =
+         event.type === "mousemove"
+            ? event.clientX
+            : event.changedTouches[0].clientX;
+      const finalPosition = this.updatePosition(pointerPosition);
+      this.moveSlide(finalPosition);
+   }
+
+   onEnd(event) {
+      const movetype = event.type === "mouseup" ? "mousemove" : "touchmove";
+      this.wrapper.removeEventListener(movetype, this.onMove);
+      this.dist.finalPosition = this.dist.movePosition;
+      this.transition(true);
+      this.changeSlideOnEnd();
+   }
+
+   changeSlideOnEnd() {
+      if (this.dist.movement > 120 && this.index.next !== undefined) {
+         this.activeNextSlide();
+      } else if (this.dist.movement < -120 && this.index.prev !== undefined) {
+         this.activePrevSlide();
+      } else {
+         this.changeSlide(this.index.active);
+      }
+   }
+
+   addSlideEvents() {
+      this.wrapper.addEventListener("mousedown", this.onStart);
+      this.wrapper.addEventListener("touchstart", this.onStart);
+      this.wrapper.addEventListener("mouseup", this.onEnd);
+      this.wrapper.addEventListener("touchend", this.onEnd);
+   }
+
+   bindEvents() {
+      this.onStart = this.onStart.bind(this);
+      this.onMove = this.onMove.bind(this);
+      this.onEnd = this.onEnd.bind(this);
+   }
+
+   // Slides config
+
    slidePosition(slide) {
       const margin = (this.wrapper.offsetWidth - slide.offsetWidth) / 2;
       return -(slide.offsetLeft - margin);
    }
 
-   slideConfig() {
+   slidesConfig() {
       this.slideArray = [...this.slide.children].map((element) => {
          const position = this.slidePosition(element);
-         console.log(position);
-         return {
-            position,
-            element,
-         };
+         return { position, element };
       });
    }
 
@@ -43,107 +99,27 @@ class Slide {
    changeSlide(index) {
       const activeSlide = this.slideArray[index];
       this.moveSlide(activeSlide.position);
-      this.slideIndexNav(index);
+      this.slidesIndexNav(index);
       this.dist.finalPosition = activeSlide.position;
+   }
+
+   activePrevSlide() {
+      if (this.index.prev !== undefined) this.changeSlide(this.index.prev);
+   }
+
+   activeNextSlide() {
+      if (this.index.next !== undefined) this.changeSlide(this.index.next);
    }
 
    init() {
       this.bindEvents();
+      this.transition(true);
       this.addSlideEvents();
-      this.slideConfig();
+      this.slidesConfig();
       return this;
-   }
-
-   activePrevSlide() {
-      console.log(this.index);
-      this.changeSlide(this.index.prev);
-
-      if (this.index.prev !== undefined) {
-         this.changeSlide(this.index.prev);
-      }
-   }
-
-   activeNextSlide() {
-      console.log(this);
-      if (this.index.prev !== undefined) {
-         this.changeSlide(this.index.next);
-      }
-   }
-
-   moveSlide(distX) {
-      this.dist.movePosition = distX;
-      this.slide.style.transform = `translate3d(${distX}px,0,0)`;
-
-      // salvar a distancia pra não ficar reiniciando toda vez
-   }
-
-   updatePosition(clientX) {
-      // basta apenas multiplicar o movement para deixar o slider mais rápido
-      this.dist.movement = (clientX - this.dist.startX) * 1.6;
-      return this.dist.finalPosition + this.dist.movement;
-   }
-
-   onMove(event) {
-      const pointerPosition =
-         event.type === "mousemove"
-            ? event.clientX
-            : event.changedTouchs[0].clientX;
-
-      const finalPosition = this.updatePosition(pointerPosition);
-      console.log(finalPosition);
-      this.moveSlide(finalPosition);
-   }
-
-   onStart(event) {
-      let moveType;
-
-      if (event.type === "mousedown") {
-         event.preventDefault();
-         this.dist.startX = event.clientX;
-         moveType = "mousemove";
-      } else {
-         this.dist.startX = event.changedTouchs[0].clientX;
-         // o touchEvent pode ter mais de um dedo então é um pouco diferente do mouse
-         moveType = "touchmove";
-      }
-      console.log(event);
-
-      this.slide.style.cursor = "grab";
-      this.wrapper.addEventListener(moveType, this.onMove);
-   }
-
-   changeSlideOnEnd() {
-      console.log(this.dist.movement);
-      if (this.dist.movement >= 120) {
-         this.activeNextSlide();
-      } else if (this.dist.movement <= -120) {
-         this.activePrevSlide();
-      }
-   }
-
-   onEnd(event) {
-      this.slide.style.cursor = "default";
-
-      this.dist.finalPosition = this.dist.movePosition;
-
-      this.wrapper.removeEventListener("mousemove", this.onMove);
-      this.changeSlideOnEnd();
-   }
-
-   addSlideEvents() {
-      this.wrapper.addEventListener("mousedown", this.onStart);
-      // this.wrapper.addEventListener("touchstart", this.onStart);
-
-      this.wrapper.addEventListener("mouseup", this.onEnd);
-      // this.wrapper.addEventListener("touchend", this.onEnd);
-   }
-
-   bindEvents() {
-      this.onStart = this.onStart.bind(this);
-      this.onMove = this.onMove.bind(this);
-      this.onEnd = this.onEnd.bind(this);
    }
 }
 
 const slide = new Slide(".slide", ".slide-wrapper");
-console.log(slide);
+slide.init();
+slide.changeSlide(3);

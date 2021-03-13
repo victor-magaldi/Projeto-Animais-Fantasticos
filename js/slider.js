303d13bb596b1fd4b8bf32cadf71179cd8a1ad08
Victor de Souza Magaldi <victmagaldi@hotmail.com>
2021-03-13 20:00:22 -0300

    adicitona custom controls e customiza arrows

--- js/slider.js
+++ js/slider.js
@@ -209,4 +209,4 @@ class SlideNav extends Slide {
 const slide = new SlideNav(".slide", ".slide-wrapper");
 slide.init();
 slide.addArrow(".prev", ".next");
-slide.addControl();
+slide.addControl(".custom-controls");
